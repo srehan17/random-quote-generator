930c18475291a77cddc6bcd4d194e6c4101656bc
Sarah Rehan <sarahahmad17@gmail.com>
2023-08-12 01:51:58 -0400

    Developed app to meet user stories 1-11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo } from "react"
+import data from "./data/data.json"
+import "./App.css"
 
-function App() {
+const App = () => {
+
+  const handleGenerateNewQuoteClick = () => { console.log('click')
+  window.location.reload()  }
+
+  const randomQuoteAndAuthor = useMemo ( () => {
+    const dataLength = data.data.length
+    const randomIndex = Math.floor(Math.random() * dataLength)   
+    const quote = JSON.stringify(data.data[randomIndex].quote)
+    const author = JSON.stringify(data.data[randomIndex].author).replace(/"/g, "")
+    return [quote, author]
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div id="quote-box">
+        <div id="text" style={{fontSize: 20, fontWeight: 600}}>Random Quote</div>
+        <div className="quoteContainer" style={{height: 100}}>
+          <div id="quote" >{randomQuoteAndAuthor[0]}</div>
+          <div id="author" style={{fontSize: 16, fontWeight: 400, marginTop: 20}}>by {randomQuoteAndAuthor[1]}</div>
+        </div>
+        <button onClick={handleGenerateNewQuoteClick} id="new-quote">Generate New Quote</button>
+        <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">Tweet Quote</a>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
